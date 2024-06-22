84b233490591dcd0ffbaf8407b633383e664fac0
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-22 16:25:22 +0300

    Complete Ui

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue,.white]),
+            LinearGradient(gradient: Gradient(colors: [.blue,Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -32,6 +32,35 @@ struct ContentView: View {
                         .font(.system(size: 70,weight: .medium))
                         .foregroundStyle(.white)
                     
+                }.padding(.bottom,40)
+                
+            
+                HStack(spacing:20){
+                    WeatherDayView(dayWeek: "TUE",
+                                 imageName: "cloud.sun.fill",
+                                 temperature: 74)
+                    WeatherDayView(dayWeek: "WED",
+                                 imageName: "sun.max.fill",
+                                 temperature: 88)
+                    WeatherDayView(dayWeek: "THU",
+                                 imageName: "wind.snow",
+                                 temperature: 55)
+                    WeatherDayView(dayWeek: "FRI",
+                                 imageName: "sunset.fill",
+                                 temperature: 60)
+                    WeatherDayView(dayWeek: "SAT",
+                                 imageName: "snow",
+                                 temperature: 25)
+                }
+                Spacer()
+                Button{
+                    print("Tapped")
+                }label: {
+                    Text("Change Day Time")
+                        .frame(width: 280,height: 50)
+                        .background(.white)
+                        .font(.system(size: 20,weight: .bold,design: .default))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
                 Spacer()
             }
@@ -43,4 +72,26 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    var dayWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayWeek)
+                .font(.system(size: 16,weight: .medium))
+                .foregroundStyle(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40,height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28,weight: .medium))
+                .foregroundStyle(.white)
+        }
+    }
 }
