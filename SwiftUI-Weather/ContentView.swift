4976ae8a0094d575b28b132f93c11ca149b30990
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-22 15:50:22 +0300

    Add initial city name, image and degree

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue,.white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32,weight: .medium))
+                    .foregroundColor(.white)
+                    .padding()
+               
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180,height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70,weight: .medium))
+                        .foregroundStyle(.white)
+                    
+                }
+                Spacer()
+            }
+            
         }
-        .padding()
+      
     }
 }
 
