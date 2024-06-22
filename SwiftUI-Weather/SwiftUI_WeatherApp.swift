34996c5eb651adfbd844faba0790aa4aaefa12c1
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-22 15:20:14 +0300

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Mahan Kheirollahi on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
